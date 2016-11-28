1b80a881947849ac17d6742f75021e5941308e9a
Marcos Valle <marcos.valle01@gmail.com>
2016-11-28 12:34:18 -0200

    Qualified!

--- cryptoPals/chall6.py
+++ cryptoPals/chall6.py
@@ -1,5 +1,7 @@
 # Break repeating-key XOR
 import binascii
+from itertools import cycle
+import chall3
 
 # Can we improve it? I bet we do :)
 def hammingDist(m1, m2):
@@ -12,26 +14,45 @@ def hammingDist(m1, m2):
             diff += 1
 
     return diff
-
-def findKeysize(ct):
+    
+def findKeysizeBySize(ct, size):
     key_dist = []
     for KEYSIZE in range(2,41):
         a = ct[0 : KEYSIZE]
-        b = ct[KEYSIZE : 2*KEYSIZE]
+        avg = 0
+        for i in range(2,size+2):
+            b = ct[(i-1)*KEYSIZE : i*KEYSIZE]
 
 # Can we get the keysize in place, without key_dist?
-        dist=0
-        for x,y in zip(a,b):
-            dist += hammingDist( bytes([x]), bytes([y]) )
+            avg += hammingDist(a,b)/(size+2)
+        key_dist.append( (KEYSIZE, avg/KEYSIZE) )
 
-        key_dist.append( (KEYSIZE, dist/KEYSIZE) )
+    return sorted(key_dist, key=lambda tup: tup[1])
+ 
+def newBlocks(ct, KEYSIZE):
+    blocks = []
+    for i in range(0, KEYSIZE):
+        block = b''
+        for b in ct[i::KEYSIZE]:
+            block += bytes([b])
+        blocks.append(block)
+    return blocks
 
-    return sorted(key_dist, key=lambda tup: tup[1])[0][0]
-     
+def breakRepSingByteXor(ct):
+    KEYSIZE = findKeysizeBySize(ct, 10)[0][0]
+    nBlock = newBlocks(ct,KEYSIZE)
+
+    key = ''
+    for block in nBlock:
+        key += chr(chall3.breakSingleByteXor(block)[0][1])
+
+    pt = "".join(chr(ord(a)^b) for a,b in zip(cycle(key), ct))
+    return key,pt
+
 ct64 = ''
 fname = "6.txt"
 with open(fname) as f:
     ct64 = f.read().strip()
     
 ct = binascii.a2b_base64(ct64)
-print( findKeysize(ct) )
+print(breakRepSingByteXor(ct))
