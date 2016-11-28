c3eac4e23ea8e572391906ecf16bf5a55d3e9f4c
Marcos Valle <marcos.valle01@gmail.com>
2016-11-28 09:58:38 -0200

    Find keysize method implemented

--- cryptoPals/chall6.py
+++ cryptoPals/chall6.py
@@ -1,4 +1,5 @@
 # Break repeating-key XOR
+import binascii
 
 # Can we improve it? I bet we do :)
 def hammingDist(m1, m2):
@@ -12,7 +13,25 @@ def hammingDist(m1, m2):
 
     return diff
 
-m1 = b'this is a test'
-m2 = b'wokka wokka!!!'
+def findKeysize(ct):
+    key_dist = []
+    for KEYSIZE in range(2,41):
+        a = ct[0 : KEYSIZE]
+        b = ct[KEYSIZE : 2*KEYSIZE]
 
-print(hammingDist(m1,m2))
+# Can we get the keysize in place, without key_dist?
+        dist=0
+        for x,y in zip(a,b):
+            dist += hammingDist( bytes([x]), bytes([y]) )
+
+        key_dist.append( (KEYSIZE, dist/KEYSIZE) )
+
+    return sorted(key_dist, key=lambda tup: tup[1])[0][0]
+     
+ct64 = ''
+fname = "6.txt"
+with open(fname) as f:
+    ct64 = f.read().strip()
+    
+ct = binascii.a2b_base64(ct64)
+print( findKeysize(ct) )
